0a00d50d7cedd4c4a2ccdf6cc6ba7cf92da94e1b
Florents Tselai <mrfloce@gmail.com>
2012-10-28 10:32:11 +0200

    delete old package

--- src/main/java/algorithms/moves/Movable.java
+++
@@ -1,5 +0,0 @@
-package algorithms.moves;
-
-public interface Movable extends Comparable<Movable>{
-	double cost();
-}
--- src/main/java/algorithms/moves/Move.java
+++
@@ -1,9 +0,0 @@
-package algorithms.moves;
-
-import model.Solution;
-
-import com.google.common.base.Function;
-
-interface Move extends Function<Movable, Neighborhood> {
-	MoveConfiguration getConfiguration();
-}

8395f57feff986c7ff726e484e90b968dbcf8a35
Florents Tselai <mrfloce@gmail.com>
2012-10-28 18:25:52 +0200

    prepare TCluster test

--- src/test/java/algorithms/construction/TClusterTest.java
+++ src/test/java/algorithms/construction/TClusterTest.java
@@ -2,19 +2,23 @@ package algorithms.construction;
 
 import static org.junit.Assert.*;
 
+import model.TTRP;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TClusterTest {
 
+	TTRP ttrp;
+	
 	@Before
 	public void setUp() throws Exception {
-		
+		ttrp = TTRP.createInstanceFromFile(TTRP.INPUT_FILE_PATH);
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertTrue(ttrp.getAllNodes().size() != 0);
 	}
 
 }

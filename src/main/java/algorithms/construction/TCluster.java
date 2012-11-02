f9303ba50c9e38f0009a8529aa236e2c8818ffe0
Florents Tselai <mrfloce@gmail.com>
2012-11-02 21:28:43 +0200

    add more description

--- src/main/java/algorithms/construction/TCluster.java
+++ src/main/java/algorithms/construction/TCluster.java
@@ -6,19 +6,20 @@ import model.TTRP;
  * Implement the T-Cluster construction heuristics as described in [3]
  * 
  * T-Cluster can be considered as a 
- * cluster-based sequential isntertion procedure, where routes are constructed one by one up to full vehicle
+ * cluster-based sequential insertion procedure, where routes are constructed one by one up to full vehicle
  * utilization.
  * 
- * A new route is initialized with the unrouted custome rus farthetst away from the depot and the
- * unused vehicle having mazimum total capacity. Thereby, complete vehicles are always preferred over
- * pure trucks. In cas of a complete vehicle, if the seed customer is a VC customer, then it is inserted into
+ * A new route is initialized with the unrouted customers farthetst away from the depot and the
+ * unused vehicle having maximum total capacity. Thereby, complete vehicles are always preferred over
+ * pure trucks. In case of a complete vehicle, if the seed customer is a VC customer, then it is inserted into
  * the main-tour. On the other hand it is inserted into a new sub-tour to the depot, if it is a TC customer.
  * 
- * The next custoemr for insertion into the routes is thne selected from the unrouted customers minimizing
+ * The next customer for insertion into the routes is thne selected from the unrouted customers minimizing
  * 		e(k) = c(ku) + c(kf) - πc(0k)
  * 		where
  * 		k specifies the customer under consideration
  * 		f the nearest customer already routed in this route, and 0 the depot. 
+ * 
  * For customer k the formulae can be explained as follows:
  * 
  * 		The first term c(ku) measures the distance to the seed customer u of this route. This distance should be 
@@ -31,6 +32,14 @@ import model.TTRP;
  * 		Finally, the third term πc(0k) can be considered as a diversification term. Varyin the parameter π will result
  * 		in different selection strategies: the higher the value of π, the higher the tendency for a selection of a
  * 		customer located far away from the depot.
+ * 
+ * Having specified the next customer k, a feasible insertion into the route is evaluated via cheapest 
+ * insertion. In case of a CVR, VC customers are forced into the main-tour and TC customers into a new 
+ * or existing sub-tour. Every VC customer on the main-tour and the depot can thereby be selected as a
+ * trailer-parking place for a new sub-tour. If the insertion of customer k would result in a violation of
+ * total vehicle capacity or maximum route length, a new route is initialized and the customer k remains
+ * unrouted. Only the last route (and its sub-tours in case of a CVR) are allowed to become infeasible if no
+ * more vehicle are available.
  * 		 
  */
 

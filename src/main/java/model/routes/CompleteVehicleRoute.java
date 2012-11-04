7f999763a4be1b1ad3bef81a3d46c096c48486a1
Florents Tselai <mrfloce@gmail.com>
2012-11-04 03:06:46 +0200

    add SubTour support

--- src/main/java/model/routes/CompleteVehicleRoute.java
+++ src/main/java/model/routes/CompleteVehicleRoute.java
@@ -1,16 +1,30 @@
 package model.routes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import model.fleet.CompleteVehicle;
 import model.nodes.Depot;
 
 public class CompleteVehicleRoute extends PureVehicleRoute {
 
+	private Set<SubTour> subTours = new HashSet<SubTour>();
+	
 	public CompleteVehicleRoute(Depot d, CompleteVehicle vehicle) {
 		super(d, vehicle);
 	}
 
 	public CompleteVehicleRoute(Depot d) {
 		super(d);
+	}
+	
+	public double cost() {
+		// TODO Implement it!
+		return 0;
+	}
+	
+	public void addSubTour(SubTour st) {
+		this.subTours.add(st);
 	}
 
 }
---
+++ src/main/java/model/routes/SubTour.java
@@ -0,0 +1,11 @@
+package model.routes;
+
+import model.nodes.Location;
+
+public class SubTour {
+	private Location root;
+	
+	public SubTour(Location root) {
+		this.root = root;
+	}
+}

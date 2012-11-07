f73bcb2fd6be3dd9a39d2a9cecf9f09208986199
Florents Tselai <mrfloce@gmail.com>
2012-11-07 19:16:40 +0200

    add Or-Opt

---
+++ src/main/java/algorithms/improvement/OrOpt.java
@@ -0,0 +1,20 @@
+package algorithms.improvement;
+
+public class OrOpt implements Move {
+
+	private MoveConfiguration configuration;
+	
+	public OrOpt() {
+		
+	}
+	
+	public Neighborhood apply(Movable input) {
+		return null;
+	}
+
+	@Override
+	public MoveConfiguration getConfiguration() {
+		return null;
+	}
+
+}
---
+++ src/test/java/algorithms/improvement/OrOptTest.java
@@ -0,0 +1,34 @@
+package algorithms.improvement;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrOptTest extends MoveTest {
+
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	@Test
+	public void testIntraRoute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Test
+	public void testApplyToSolution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

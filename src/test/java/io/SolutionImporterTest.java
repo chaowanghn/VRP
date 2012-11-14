80ed2c2195348662f1cd2d94256fa23d16b5e2d1
Florents Tselai <mrfloce@gmail.com>
2012-11-14 14:31:09 +0200

    add solution importer testcase

---
+++ src/test/java/io/SolutionImporterTest.java
@@ -0,0 +1,20 @@
+package io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionImporterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+}

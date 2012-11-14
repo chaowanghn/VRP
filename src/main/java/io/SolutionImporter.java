3d4c7bcfb9e65412534a375c60be3e9506719fad
Florents Tselai <mrfloce@gmail.com>
2012-11-14 14:55:29 +0200

    add method to read the basic infos

--- src/main/java/io/SolutionImporter.java
+++ src/main/java/io/SolutionImporter.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.base.Splitter;
+
 import model.TTRP;
 import model.fleet.*;
 import model.nodes.*;
@@ -16,6 +18,10 @@ public class SolutionImporter {
 	private int numberOfVehicleRoutes;
 	private int numberOfTruckRoutes;
 	private int numberOfSubTours;
+	private int numberOfTrucksUsed;
+	private int numberOfTrailersUsed; 
+	
+	private List<String> fileLines;
 	
 	
 
@@ -39,6 +45,13 @@ public class SolutionImporter {
 		return null;
 	}
 
+	private void readBasicInfo(List<String> lines){
+		this.totalCost = Double.parseDouble(lines.get(0).split(" ")[2]);
+		this.numberOfVehicleRoutes = getIntFromString(lines.get(1));
+		this.numberOfTruckRoutes = getIntFromString(lines.get(2));
+		this.numberOfSubTours = getIntFromString(lines.get(3));		
+	}
+	
 	public static int getIntFromString(String str){
 		Pattern intsOnly = Pattern.compile("\\d+");
 		Matcher makeMatch = intsOnly.matcher(str);

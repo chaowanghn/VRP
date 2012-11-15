e3eb327c1f7b79da3336b805ecbb0b2e62853569
Florents Tselai <mrfloce@gmail.com>
2012-11-16 01:45:39 +0200

    add hashcode and equals for Edge

--- src/main/java/model/routes/Edge.java
+++ src/main/java/model/routes/Edge.java
@@ -1,5 +1,7 @@
 package model.routes;
 
+import com.google.common.base.Objects;
+
 import model.nodes.Node;
 
 public class Edge implements Comparable<Edge>{
@@ -30,4 +32,32 @@ public class Edge implements Comparable<Edge>{
 			return 0;
 		}
 	}
+	
+	@Override
+	public int hashCode(){
+		return Objects.hashCode(n1,n2);
+	}
+	
+	public boolean equals(Edge other){
+		return other.n1.equals(this.n1) && other.n2.equals(this.n2);
+	}
+
+	
+	public Node getN1() {
+		return n1;
+	}
+
+	public void setN1(Node n1) {
+		this.n1 = n1;
+	}
+
+	public Node getN2() {
+		return n2;
+	}
+
+	public void setN2(Node n2) {
+		this.n2 = n2;
+	}
+
+	
 }

991c308b6aee8cfb38c99cee963a79ae7fbb583e
Florents Tselai <mrfloce@gmail.com>
2012-11-16 00:13:16 +0200

    add reference on VRPs moves

--- src/main/java/algorithms/improvement/Move.java
+++ src/main/java/algorithms/improvement/Move.java
@@ -2,6 +2,9 @@ package algorithms.improvement;
 
 import com.google.common.base.Function;
 
+/*
+ * For an overview of simple moves used in VRPs see [7]
+ */
 interface Move extends Function<Movable, Neighborhood> {
 	MoveConfiguration getConfiguration();
 }

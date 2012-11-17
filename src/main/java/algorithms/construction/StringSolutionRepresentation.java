0cc30ed6f2f3dd2b42632b13670e1c47c3f0d182
Florents Tselai <mrfloce@gmail.com>
2012-11-17 19:00:54 +0200

    add Lin et.al. construction heuristic

---
+++ src/main/java/algorithms/construction/StringSolutionRepresentation.java
@@ -0,0 +1,20 @@
+package algorithms.construction;
+
+import model.Solution;
+import model.TTRP;
+
+/*
+ * Implements the construction heuristic based on a String representation of a TTRP solution
+ * For implementation details see [9]
+ */
+
+public class StringSolutionRepresentation implements ConstructionHeuristic{
+
+	@Override
+	public Solution apply(TTRP input) {
+		return null;
+		
+	}
+
+
+}

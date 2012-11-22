b9bdf88d135a4f2a22353503f60368a19f2d9371
Florents Tselai <mrfloce@gmail.com>
2012-11-22 05:30:07 +0200

    Refactoring: extract class

--- src/main/java/algorithms/construction/StringSolutionRepresentation.java
+++ src/main/java/algorithms/construction/StringSolutionRepresentation.java
@@ -162,14 +162,6 @@ public class StringSolutionRepresentation implements ConstructionHeuristic{
 		Collections.shuffle(this.permutation);
 	}
 	
-	public static enum ServiceType{
-		COMPLETE_VEHICLE,TRUCK;
-		private static final List<ServiceType> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
-		private static final int SIZE = VALUES.size();
-		
-		public static ServiceType randomServiceType(Random random)  {
-			return VALUES.get(random.nextInt(SIZE));
-		}
-	}
+
 	
 }
